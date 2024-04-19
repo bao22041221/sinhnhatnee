ddb2e4d38dfbb1e54bb8323aab21f749f583ec98
bao22041221 <davidbaolienminh1@gmail.com>
2024-04-19 23:04:02 +0700

    add file html, css, js

---
+++ config.js
@@ -0,0 +1,9 @@
+const nameGirl = 'Cô Ms.Nhung';
+const giftUrl = 'https://www.facebook.com/profile.php?id=100030530376548&mibextid=ZbWKwL&gidzl=34DHAa3L-de9NrOaHCdzUGvKKIHSnQXq5LWCBrp5yd0NLbj_MPVoS1S7Noe8bVicG5HNUJPyf9rfGjBwSW';
+const eventName = 'Happy Birth Day🎂'
+const titleCard = 'Tặng teacher Ms.Nhung';
+const contentCard = 'Chúc cô sinh nhật vui vẻ nha 🎂, love my teacher very much 😽';
+
+const giftImage = 'z5344214705603_15261ffb2f63a581c9f216a0571d2e28.jpg';
+const base64 = '';
+const giftImageBase64 = "z5344215723437_3b7f75f4ba4342fd8a70ba9966810295.jpg, " + base64;
